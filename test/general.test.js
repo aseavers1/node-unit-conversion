0bb984bc7eed166542e0467f2778d432dc497ca0
Teodor Matis <teodor.matis@diamondconsulting.ro>
2016-06-30 10:18:36 +0300

    traaaviiiis!

--- test/general.test.js
+++ test/general.test.js
@@ -55,5 +55,10 @@ module.exports = {
 	'uk-imp-lengths': function(test) {
 		test.equal(convert(10).from('uk_inch').to('uk_foot'), 0.8333333333);
 		test.done();
+	},
+
+	'inter-scale-lengths': function(test) {
+		test.equal(convert(10).from('uk_inch').to('metre'), 0.254);
+		test.done();
 	}
 }
